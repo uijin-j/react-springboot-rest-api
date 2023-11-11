9282dcac89569f3d15899a141185ad8a2da3fe89
uijin-j <uijin0531@gmail.com>
2023-11-12 00:31:24 +0900

    fix: Auditing이 안되는 오류 해결

--- shop_gamza/src/main/java/org/devcourse/shop_gamza/ShopGamzaApplication.java
+++ shop_gamza/src/main/java/org/devcourse/shop_gamza/ShopGamzaApplication.java
@@ -2,7 +2,9 @@ package org.devcourse.shop_gamza;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ShopGamzaApplication {
 
