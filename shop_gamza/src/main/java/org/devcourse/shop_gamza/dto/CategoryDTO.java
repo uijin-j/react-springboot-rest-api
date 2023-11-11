98f17fbafa44ebb45df51e141b2b8cf96d246191
uijin-j <uijin0531@gmail.com>
2023-11-12 04:30:43 +0900

    refactor: 불필요한 레코드 삭제

--- shop_gamza/src/main/java/org/devcourse/shop_gamza/dto/CategoryDTO.java
+++
@@ -1,9 +0,0 @@
-package org.devcourse.shop_gamza.dto;
-
-import org.devcourse.shop_gamza.domain.category.Category;
-
-public record CategoryDTO (Long id, String name) {
-    public static CategoryDTO of(Category category) {
-        return new CategoryDTO(category.getId(), category.getName());
-    }
-}
