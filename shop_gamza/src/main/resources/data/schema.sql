91a3b456c25958be00c6f16bf1758437376aca83
uijin-j <uijin0531@gmail.com>
2023-11-12 00:30:35 +0900

    docs: 데이터 베이스 테이블명 변경에 따른 schema.sql 수정

--- shop_gamza/src/main/resources/data/schema.sql
+++ shop_gamza/src/main/resources/data/schema.sql
@@ -6,12 +6,12 @@ CREATE DATABASE test_gamza_database;
 
 USE gamza_database;
 
-DROP TABLE IF EXISTS product_images;
-DROP TABLE IF EXISTS images;
-DROP TABLE IF EXISTS products;
-DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS product_image;
+DROP TABLE IF EXISTS image;
+DROP TABLE IF EXISTS product;
+DROP TABLE IF EXISTS category;
 
-CREATE TABLE categories (
+CREATE TABLE category (
     category_id     BIGINT AUTO_INCREMENT,
     name            VARCHAR(20) NOT NULL,
     created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -19,7 +19,7 @@ CREATE TABLE categories (
     CONSTRAINT pk_category_id PRIMARY KEY (category_id)
 );
 
-CREATE TABLE products (
+CREATE TABLE product (
     product_id      BIGINT AUTO_INCREMENT,
     name            VARCHAR(20) NOT NULL,
     price           INT NOT NULL,
@@ -31,10 +31,10 @@ CREATE TABLE products (
     updated_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     CONSTRAINT pk_product_id PRIMARY KEY (product_id),
     CONSTRAINT fk_category_id
-        FOREIGN KEY (category_id) REFERENCES categories(category_id)
+        FOREIGN KEY (category_id) REFERENCES category(category_id)
 );
 
-CREATE TABLE images (
+CREATE TABLE image (
     image_id            BIGINT AUTO_INCREMENT,
     upload_file_name    VARCHAR(255),
     store_file_name     VARCHAR(255) NOT NULL,
@@ -43,7 +43,7 @@ CREATE TABLE images (
     CONSTRAINT pk_image_id PRIMARY KEY (image_id)
 );
 
-CREATE TABLE product_images (
+CREATE TABLE product_image (
     product_image_id    BIGINT AUTO_INCREMENT,
     is_cover_image      TINYINT DEFAULT false,
     product_id          BIGINT NOT NULL,
@@ -52,20 +52,20 @@ CREATE TABLE product_images (
     updated_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     CONSTRAINT pk_product_image_id PRIMARY KEY (product_image_id),
     CONSTRAINT fk_product_id
-        FOREIGN KEY (product_id) REFERENCES products(product_id),
+        FOREIGN KEY (product_id) REFERENCES product(product_id),
     CONSTRAINT fk_image_id
-        FOREIGN KEY (image_id) REFERENCES images(image_id)
+        FOREIGN KEY (image_id) REFERENCES image(image_id)
 );
 
 #  테스트 데이터 베이스 DDL
 USE test_gamza_database;
 
-DROP TABLE IF EXISTS product_images;
-DROP TABLE IF EXISTS images;
-DROP TABLE IF EXISTS products;
-DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS product_image;
+DROP TABLE IF EXISTS image;
+DROP TABLE IF EXISTS product;
+DROP TABLE IF EXISTS category;
 
-CREATE TABLE categories (
+CREATE TABLE category (
     category_id     BIGINT AUTO_INCREMENT,
     name            VARCHAR(20) NOT NULL,
     created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -73,7 +73,7 @@ CREATE TABLE categories (
     CONSTRAINT pk_category_id PRIMARY KEY (category_id)
 );
 
-CREATE TABLE products (
+CREATE TABLE product (
     product_id      BIGINT AUTO_INCREMENT,
     name            VARCHAR(20) NOT NULL,
     price           INT NOT NULL,
@@ -85,10 +85,10 @@ CREATE TABLE products (
     updated_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     CONSTRAINT pk_product_id PRIMARY KEY (product_id),
     CONSTRAINT fk_category_id
-      FOREIGN KEY (category_id) REFERENCES categories(category_id)
+      FOREIGN KEY (category_id) REFERENCES category(category_id)
 );
 
-CREATE TABLE images (
+CREATE TABLE image (
     image_id            BIGINT AUTO_INCREMENT,
     upload_file_name    VARCHAR(255),
     store_file_name     VARCHAR(255) NOT NULL,
@@ -97,7 +97,7 @@ CREATE TABLE images (
     CONSTRAINT pk_image_id PRIMARY KEY (image_id)
 );
 
-CREATE TABLE product_images (
+CREATE TABLE product_image (
     product_image_id    BIGINT AUTO_INCREMENT,
     is_cover_image      TINYINT DEFAULT false,
     product_id          BIGINT NOT NULL,
@@ -106,7 +106,7 @@ CREATE TABLE product_images (
     updated_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     CONSTRAINT pk_product_image_id PRIMARY KEY (product_image_id),
     CONSTRAINT fk_product_id
-        FOREIGN KEY (product_id) REFERENCES products(product_id),
+        FOREIGN KEY (product_id) REFERENCES product(product_id),
     CONSTRAINT fk_image_id
-        FOREIGN KEY (image_id) REFERENCES images(image_id)
+        FOREIGN KEY (image_id) REFERENCES image(image_id)
 );
